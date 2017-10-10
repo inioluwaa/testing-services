1a17091902338fb617ced118477c4a04855e9160
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-10-10 11:45:24 +0100

    injecting services into services

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -7,7 +7,7 @@ import { AccountsService } from 'app/accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
   accountAdded = new EventEmitter<{name: string, status: string}>();
@@ -17,6 +17,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsSerrvice.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
